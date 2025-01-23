a5bbe3a38972d3a098d94d64cbe5fe15844f220b
Rashid T Tahasildar <85626896+Tahasildarrashid@users.noreply.github.com>
2025-01-23 22:55:07 +0530

    Delete src/main directory

--- src/main/java/assignment1/exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package assignment1.exceptions;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}

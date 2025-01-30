efbfc5af1dc2ff0e12b82300ed7af6d647c7780e
Rashid T Tahasildar <85626896+Tahasildarrashid@users.noreply.github.com>
2025-01-30 12:23:14 +0530

    Delete bookapp directory

--- bookapp/src/main/java/com/bookapp/loggable/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.bookapp.loggable;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-@Target(java.lang.annotation.ElementType.METHOD)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-public @interface Logger {
-
-}
